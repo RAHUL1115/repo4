b6ca10903b5052999a4a380cbb251b4e573e812c
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-09 11:53:52 +0530

    added version logging of custom packages

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // require repo1
-const repo1 = require('@rahul1115/repo1')
+const repo2 = require('@rahul1115/repo2');
+const repo3 = require('@rahul1115/repo3');
 
 // console log the contents
-console.log(repo1);
+console.log(repo2.name,':',repo2.version);
+console.log(repo3.name,':',repo3.version);
